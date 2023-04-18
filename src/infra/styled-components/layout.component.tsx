7d8692f4f45d90b33c9d1e74e1ee3aae9a1a2748
Bryan Arroyave Ortiz <barroyave@leal.co>
2023-04-18 09:49:13 -0500

    Create layout

---
+++ src/infra/styled-components/layout.component.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const LayoutContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding: 0;
+  gap: 1rem;
+  @media only screen and (max-width: 1176px) {
+    padding: 12px;
+  }
+`;
